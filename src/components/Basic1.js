fa6991025599076ac56e28ab3ee753536906dcb5
Sawako Yamagishi <tokage@SawakonoMacBook-Pro.local>
2021-02-24 22:02:05 +0900

    useState,useEffectの使い方

---
+++ src/components/Basic1.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+
+const Basic1 = () => {
+
+  const [product, setProducts] = useState({name: '', price: ''});
+
+  return (
+    <>
+      <form>
+        <input type='text' value={product.name}
+        onChange={evt => setProducts({...product, name: evt.target.value})}></input>
+        <input type='text' value={product.price}
+        onChange={evt => setProducts({...product, price: evt.target.value})}></input>
+
+      </form>
+      <h3>Product name is {product.name}</h3>
+      <h3>Product price is {product.price}</h3>
+    </>
+  )
+}
+
+export default Basic1;
